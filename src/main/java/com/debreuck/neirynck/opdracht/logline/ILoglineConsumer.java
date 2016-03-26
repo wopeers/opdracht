4004cd93237dfa68e81ec74c1fed48e09ea7257c
wopeers <wouter@peers.cx>
2016-03-26 13:42:48 +0100

    defined ILoglineConsumer + IReportGenerator

---
+++ src/main/java/com/debreuck/neirynck/opdracht/logline/ILoglineConsumer.java
@@ -0,0 +1,6 @@
+package com.debreuck.neirynck.opdracht.logline;
+
+import java.util.function.Consumer;
+
+public interface ILoglineConsumer extends Consumer<Logline> {
+}
---
+++ src/main/java/com/debreuck/neirynck/opdracht/report/IReportGenerator.java
@@ -0,0 +1,8 @@
+package com.debreuck.neirynck.opdracht.report;
+
+import com.debreuck.neirynck.opdract.report.Report;
+
+public interface IReportGenerator {
+
+    Report generateReport();
+}
